3dab13fb77abb56321bbf7d205a2b02db5ec021a
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2026-07-10 17:32:54 +0200

    Add middleware to embed "X-Clacks-Overhead: GNU Terry Pratchett" header and CI/CD configuration

---
+++ StacyClouds.GnuSirTerry.Net/GnuSirTerryMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace StacyClouds.GnuSirTerry.Net;
+
+public class GnuSirTerryMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context
+            .Response
+            .Headers
+            .Add(new KeyValuePair<string, StringValues>(
+                "X-Clacks-Overhead", 
+                "GNU Terry Pratchett"));
+        
+        await next(context);
+    }
+}
+
+// Extension method for cleaner registration
+public static class GnuSirTerryMiddlewareExtensions
+{
+    public static IApplicationBuilder UseGnuSirTerryMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<GnuSirTerryMiddleware>();
+    }
+}
